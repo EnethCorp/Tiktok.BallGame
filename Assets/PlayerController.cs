c4df410dba8ef38420a6f03eff7a6470844f3ccf
Enething <enethingofficial@gmail.com>
2023-08-27 16:06:37 +0200

    Zum main pfushesn
    noice

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,9 +6,22 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public User parent;
+    private Rigidbody _rb;
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
 
     public void AddPoints(int amount)
     {
         parent.AddPoints(amount);
+    }
+
+    void FixedUpdate()
+    {
+        if (_rb.velocity.magnitude <= 0.1f)
+        {
+            _rb.velocity = new Vector3(Random.Range(-0.5f, 0.5f), 0f, 0f);
+        }
     }
 }
