28fbf869cc99336d08389058d4a1bf9c9c65c8af
Enething <115583828+theyluvEnething@users.noreply.github.com>
2023-09-03 17:59:14 +0200

    Revert "Added Special Gift Effects"
    This reverts commit 736c4b99a1ff30d997b3b91f4e2d3230d21c81e2.

--- Assets/ColorTransition.cs
+++ Assets/ColorTransition.cs
@@ -4,30 +4,18 @@ using UnityEngine;
 
 public class ColorTransition : MonoBehaviour
 {
-    [SerializeField] public Gradient gradient1;
+    [SerializeField] public Gradient gradient;
     [SerializeField] public Gradient gradient2;
     [SerializeField] public Material material;
 
-    private bool ColorMode = false;
-
-    void Start()
+    void Update()
     {
-        if (gradient1 == null || gradient2 == null || material == null)
+        if (gradient == null || material == null)
         {
             Debug.LogError("Gradient or Material is zero on TransitionColor Object.");
             return;
         }
-    }
 
-    void Update()
-    {
-        if (Input.GetKeyDown("1"))
-        {
-            Debug.Log("Switching Color mode");
-            ColorMode = !ColorMode;
-        }
-        Gradient gradient = (!ColorMode) ? gradient1 : gradient2;
-
-        material.color = gradient.Evaluate((Time.time / 45) % 1);
+        material.color = gradient2.Evaluate((Time.time / 45) % 1);
     }
 }
